f3a006a2e7222f11349d69a84e016fc7809e4590
Masud Rana <rannamasudrana753@gmail.com>
2026-07-31 00:03:02 +0600

    WIP:

--- src/store/ui-store.ts
+++ src/store/ui-store.ts
@@ -1,1 +1,13 @@
-export {};
+"use client";
+
+import { create } from "zustand";
+
+type UiState = {
+  dashboardMenuOpen: boolean;
+  setDashboardMenuOpen: (open: boolean) => void;
+};
+
+export const useUiStore = create<UiState>((set) => ({
+  dashboardMenuOpen: false,
+  setDashboardMenuOpen: (dashboardMenuOpen) => set({ dashboardMenuOpen }),
+}));
